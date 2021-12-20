806c0536172e3943a4b43e759625b4631292fc2a
moiz1647 <u2020540@giki.edu.pk>
2021-12-20 16:10:42 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def decrypt(data, shift):
+    new_data = ""
+    for x in data:
+        if ord(x) < 65 + shift:
+            diff = ord(x) - 65
+            new_data += chr(84 + diff)
+        else:
+            new_data += chr(ord(x) - 7)
+
+    return new_data
+
+
+# code = int(input("Enter the code to decrypt: "))
+code = "KLMLUKAOLMVYA"
+# shift = int(input("Enter the shift: "))
+shift = 7
+print(decrypt(code, shift))
+# 65 to 90
